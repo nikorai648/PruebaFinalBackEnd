f860aea3bfe32e8f027db6e48f1a26b97e14467f
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-11-30 19:46:48 -0300

    commit

--- api_django/core/models.py
+++ api_django/core/models.py
@@ -7,3 +7,5 @@ class TipoTrabajador(models.Model):
     rol_cargo = models.CharField(max_length=60)
     tipo_contrato = models.CharField(max_length=20)
 
+def __str__(self):
+        return f"{self.rol_cargo} ({self.tipo_contrato})"
