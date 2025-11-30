ad9f59c9e4902a491f7f4918782f067096c96939
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-11-30 19:41:28 -0300

    commit

--- api_django/core/models.py
+++ api_django/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-# Create your models here.
+
+
+class TipoTrabajador(models.Model):
+
+    rol_cargo = models.CharField(max_length=60)
+    tipo_contrato = models.CharField(max_length=20)
+
