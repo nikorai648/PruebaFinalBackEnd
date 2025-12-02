3d49711b0c68e9ff7864313081f8463c0ebbf03d
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-12-02 08:39:23 -0300

    commit

--- api_django/core/serializers.py
+++ api_django/core/serializers.py
@@ -1,1 +1,13 @@
 from rest_framework import serializer
+from SerialApp.models import Tipotrabajador, Trabajador, Accidente, Asistencia, EficienciaTrabjador, DesempenoTrabajador, SueldoTrabajador
+
+
+class Tipotrabajador(serializer.ModeSerializer):
+    class Meta:
+        model = Tipotrabajador
+        fields = '__all__'
+
+class Trabajador(serializer.ModeSerializer):
+    class Meta:
+        model = Trabajador
+        fields = '__all__'
