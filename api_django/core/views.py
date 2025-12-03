5e446460963cd67aac7271cade7ca7b56a22036f
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-12-03 01:01:48 -0300

    commit

--- api_django/core/views.py
+++ api_django/core/views.py
@@ -186,3 +186,17 @@ def desempeno_list(request):
         desempenos = DesempenoTrabajador.objects.all()
         serializer = DesempenoTrabajadorSerializer(desempenos, many=True)
         return Response(serializer.data)    
+    if request.method == 'POST':
+        serializer = DesempenoTrabajadorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def desempeno_detail(request, pk):
+    try:
+        desempeno = DesempenoTrabajador.objects.get(pk=pk)
+    except DesempenoTrabajador.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
