e955dc19a844d8dc908e491f65bc40a6810c14f9
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-12-02 22:33:30 -0300

    commit

--- api_django/backend/urls.py
+++ api_django/backend/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/trabajadores/', views.trabajador_list, name='trabajador_list'),
+    path('api/trabajadores/<int:pk>/', views.trabajador_detail, name='trabajador_detail'),
 ]
