6e5ba81b2e4ba492313620877ca5ed2f3f91787b
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-12-03 21:39:05 -0300

    commit

--- api_django/backend/settings.py
+++ api_django/backend/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api_django'
+    'core',
+    'rest_framework.authtoken',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
