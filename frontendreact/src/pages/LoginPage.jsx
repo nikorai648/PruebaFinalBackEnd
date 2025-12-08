2299dcde5bfce16b4bc6fede9a61ad56c40cc29f
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-12-07 22:36:54 -0300

    commit

--- frontendreact/src/pages/LoginPage.jsx
+++ frontendreact/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { login as loginApi } from "../api/fakeApi";
+import { loginApi } from "../api/auth";   // ✅ conexión real API Django
 import { AuthContext } from "../App";
 
 export default function LoginPage() {
