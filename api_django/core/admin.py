496fe6ce03a41ced7824bf6d372b84125d5e829d
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-12-10 13:35:24 -0300

    commit

--- api_django/core/admin.py
+++ api_django/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (
+    Trabajador,
+    Asistencia,
+    Accidente,
+    EficienciaTrabajador,
+    DesempenoTrabajador,
+    SueldoTrabajador
+)
 
-# Register your models here.
+admin.site.register(Trabajador)
+admin.site.register(Asistencia)
+admin.site.register(Accidente)
+admin.site.register(EficienciaTrabajador)
+admin.site.register(DesempenoTrabajador)
+admin.site.register(SueldoTrabajador)
