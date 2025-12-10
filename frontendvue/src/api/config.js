006f87801ff1a226540ce8d526e48f0593efe117
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-12-09 21:14:19 -0300

    commit

---
+++ frontendvue/src/api/config.js
@@ -0,0 +1,16 @@
+// src/api/config.js
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Token ${token}`;
+  }
+  return config;
+});
+
+export default api;
