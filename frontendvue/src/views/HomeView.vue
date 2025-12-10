56af7e9b094c32ba981a17ed481357fe56f8d4a1
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-12-09 21:28:36 -0300

    commit

--- frontendvue/src/views/HomeView.vue
+++ frontendvue/src/views/HomeView.vue
@@ -1,4 +1,3 @@
-<script setup>
 <template>
   <div class="container mt-5">
     <h1>Opciones del Sistema</h1>
@@ -21,18 +20,3 @@ export default {
   components: { RouterLink },
 };
 </script>
-</script>
-
-<template>
-  <div class="container mt-4">
-    <div class="p-4 bg-light rounded-3">
-      <h1 class="h4 mb-2">Opciones del Sistema</h1>
-      <p class="mb-4">Sistema de Trabajadores, Asistencias y Accidentes.</p>
-      <div class="d-flex gap-2 flex-wrap">
-        <RouterLink class="btn btn-primary" to="/trabajadores">
-          Trabajadores
-        </RouterLink>
-      </div>
-    </div>
-  </div>
-</template>
