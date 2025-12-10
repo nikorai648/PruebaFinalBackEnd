0dfad8cc6cf02df8b351e07ef948ed8fb1661f7c
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-12-09 21:22:09 -0300

    commit

--- frontendvue/src/App.vue
+++ frontendvue/src/App.vue
@@ -1,19 +1,42 @@
 <template>
-  <div>
-    <Navbar v-if="auth.state.isAuthenticated" />
-    <router-view />
-  </div>
+  <nav class="navbar navbar-expand navbar-dark bg-dark px-3">
+    <RouterLink class="navbar-brand" to="/">Optimizacion y Logistica</RouterLink>
+
+    <div class="navbar-nav me-auto">
+      <RouterLink class="nav-link" to="/trabajadores">Trabajadores</RouterLink>
+      <RouterLink class="nav-link" to="/asistencias">Asistencias</RouterLink>
+      <RouterLink class="nav-link" to="/accidentes">Accidentes</RouterLink>
+      <RouterLink class="nav-link" to="/eficiencias">Eficiencias</RouterLink>
+      <RouterLink class="nav-link" to="/desempenos">Desempeños</RouterLink>
+      <RouterLink class="nav-link" to="/sueldos">Sueldos</RouterLink>
+    </div>
+
+    <span class="navbar-text text-white me-3">
+      Hola, {{ auth.state.username }}
+    </span>
+
+    <button class="btn btn-outline-light btn-sm" @click="handleLogout">
+      Salir
+    </button>
+  </nav>
 </template>
 
 <script>
-import Navbar from "./components/Navbar.vue";
-import { useAuth } from "./auth";
+import { useRouter, RouterLink } from "vue-router";
+import { useAuth } from "../auth";
 
 export default {
-  components: { Navbar },
+  components: { RouterLink },
   setup() {
     const auth = useAuth();
-    return { auth };
+    const router = useRouter();
+
+    const handleLogout = () => {
+      auth.logout();
+      router.push("/login");
+    };
+
+    return { auth, handleLogout };
   },
 };
 </script>
