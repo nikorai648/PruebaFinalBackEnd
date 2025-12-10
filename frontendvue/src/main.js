a5cea3bb51507a5362b7878bc1e7a0a52ce96fdb
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-12-10 02:29:15 -0300

    commit

--- frontendvue/src/main.js
+++ frontendvue/src/main.js
@@ -1,11 +1,12 @@
 // src/main.js
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router"; // 👈 usa el router que definiste
+import router from "./router/index.js";   // 👈 IMPORTA ESTE router
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 const app = createApp(App);
 
-app.use(router);     // 👈 conecta el router
+app.use(router);
 app.mount("#app");
